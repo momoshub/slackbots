23d39d235f598e3983c6a6e9236bdbd59747d0ba
Kai <thien@momos.com>
2025-04-18 14:22:54 +0700

    Update on-call notification with additional links and user mention

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,11 @@ According to the On-Call Rotation schedule, you're on call this week.
 Please make sure you're available for on-call duties.
 
 You can check the rotation here:
-🔗 https://www.notion.so/go-momos/33e55a34a8834f5f8d8a37e29b452752?v=35cf38209c9045c8b239140040c8ffc9
+🔗 Notion: https://www.notion.so/go-momos/33e55a34a8834f5f8d8a37e29b452752?v=35cf38209c9045c8b239140040c8ffc9
+🔗 Github Queue: https://github.com/momoshub/slackbots/blob/main/queue
+🔗 Github Current: https://github.com/momoshub/slackbots/blob/main/current
+
+FYI <@S08993D1SMB>.
 
 Thanks! 🙏
 `
